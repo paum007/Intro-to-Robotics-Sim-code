be7050924a0a762fa64ac90a8f214be94c24bf7b
Pau M. Marcos <paumaria07@gmail.com>
2026-04-24 14:06:24 +0100

    Ultra gay robot

--- main_hardware.py
+++ main_hardware.py
@@ -1,4 +1,5 @@
 from pyniryo import *
+import time
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -6,7 +7,7 @@ from pyniryo import *
 if __name__=="__main__": 
 
     robot_ip_address = '10.10.10.10'
-    workspace_name = "FraserPau"  # Robot's Workspace Name
+    workspace_name = "PauFraser2"  # Robot's Workspace Name
     robot = NiryoRobot(robot_ip_address) # Connect to robot
 
     # Clear collision if detected during a previous movement
@@ -37,7 +38,7 @@ if __name__=="__main__":
 
     
     initial_joints = JointsPosition(0, 0, 0, 0, 0, 0)
-    observation_joints = JointsPosition(-0.0692,0.4039,-0.9067,-0.0152,-1.0247,-0.0443)
+    observation_joints = JointsPosition(-0.089,0.1267,-0.6885,-0.0858,-1.2901,0.0338)
     approach_joints = JointsPosition(-0.025,-0.1959,-0.6416,-0.0213,-0.7854,-0.0934)
     ready_to_place = JointsPosition(1.4587,0.2191,-0.3704,-0.0551,-1.4282,-0.1226)
     red_joints = JointsPosition(1.1102,-0.4747,-0.6143,-0.3113,-0.5262,-0.1456)
@@ -45,86 +46,94 @@ if __name__=="__main__":
     blue_joints = JointsPosition(1.4314,-0.3398,-0.734,-0.052,-0.5262,-0.1456)
     going_back = JointsPosition(0.8576,0.1661,-0.9006,0.0154,-0.8453,0.0016)
 
+    initial_pose = PoseObject(0, 0, 0, 0, 0, 0)
+    
+
     robot.run_conveyor(conveyor_id)
     robot.set_arm_max_velocity(100)
-
-    robot.move_joints(initial_joints)
+    print("Moving")
+    robot.move(initial_joints)
 
     objects = 6
 
     try:
         while objects!=0:
             
-            robot.move_joints(observation_joints)
+            robot.move(observation_joints)
             robot.open_gripper()
 
             if robot.digital_read(sensor_pin_id) == PinState.LOW:
                 robot.stop_conveyor(conveyor_id)
                 print(f"Connected: {robot.play_sound('connected.wav')}")
 
-                target_pos = robot.move_to_object(workspace_name, 0, ObjectShape.ANY, ObjectColor.ANY)
+                target_pose = robot.get_target_pose_from_cam(workspace_name, 0, ObjectShape.ANY, ObjectColor.ANY)
+                target_pose_coord = (target_pose[1])
+                print(f"Target pose: {target_pose}")
+                print(f"0: {target_pose[0]}")
+                print(f"1: {target_pose[1]}")
+                print(f"2: {target_pose[2]}")
+                print(f"3: {target_pose[3]}")
+
+                robot.move(target_pose_coord)
+                # target_pos = robot.move_to_object(workspace_name, 0, ObjectShape.ANY, ObjectColor.ANY)
                 
-                print(f"0: {target_pos[0]}")
-                print(f"1: {target_pos[1]}")
-                print(f"2: {target_pos[2]}")
                 # robot.shift_pose(RobotAxis.X, 0.007, True) # shift forward
                 # robot.shift_pose(RobotAxis.Y, 0.018, True) # shift left
                 robot.shift_pose(RobotAxis.Z, -0.01, True) # shift down
-
+                
                 robot.close_gripper()
                 
-                color = target_pos[2]
+                color = target_pose[3]
 
                 if (color == ObjectColor.RED):
                     print(f"Colour: {color}")
-                    robot.move_joints(ready_to_place)
-                    robot.move_joints(red_joints)
+                    robot.move(ready_to_place)
+                    robot.move(red_joints)
                     robot.open_gripper()
 
 
                 elif (color == ObjectColor.GREEN):
                     print(f"Colour: {color}")
-                    robot.move_joints(ready_to_place)
-                    robot.move_joints(green_joints)
+                    robot.move(ready_to_place)
+                    robot.move(green_joints)
                     robot.open_gripper()
 
 
                 elif (color == ObjectColor.BLUE):
                     print(f"Colour: {color}")
-                    robot.move_joints(ready_to_place)
-                    robot.move_joints(blue_joints)
+                    robot.move(ready_to_place)
+                    robot.move(blue_joints)
                     robot.open_gripper()
                 
-                robot.move_joints(going_back)
-                robot.move_joints(observation_joints)
+                robot.move(going_back)
+                robot.move(observation_joints)
                 robot.run_conveyor(conveyor_id)
                 
                 objects -= 1
                 
                 print(f"Objects left: {objects}")
 
-        robot.move_joints(initial_joints)
+        robot.move(initial_joints)
         robot.stop_conveyor(conveyor_id)
         robot.play_sound('booting.wav')
 
-    except KeyboardInterrupt:
-        robot.stop_conveyor(conveyor_id)
-        robot.move_joints(initial_joints)
-        robot.close_connection()
+    # except KeyboardInterrupt:
+    #     robot.unset_conveyor(conveyor_id) # Deactivating connexion with the Conveyor Belt
+    #     robot.close_connection()
 
     except Exception as e:
         robot.unset_conveyor(conveyor_id) # Deactivating connexion with the Conveyor Belt
         robot.close_connection()
         print(f"Error: {e}")
 
-    # robot.move_joints(initial_joints)
-    # robot.move_joints(observation_joints)
-    # robot.move_joints(approach_joints)
-    # robot.move_joints(ready_to_place)
-    # robot.move_joints(red_joints)
-    # robot.move_joints(green_joints)
-    # robot.move_joints(blue_joints)
-    # robot.move_joints(initial_joints)
+    # robot.move(initial_joints)
+    # robot.move(observation_joints)
+    # robot.move(approach_joints)
+    # robot.move(ready_to_place)
+    # robot.move(red_joints)
+    # robot.move(green_joints)
+    # robot.move(blue_joints)
+    # robot.move(initial_joints)
 
 
     
